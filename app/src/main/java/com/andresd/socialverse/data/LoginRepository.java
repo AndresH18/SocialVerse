43876b06094070a0292508eb11c7eb8ff26f887c
andre <andres.hoyos18@eia.edu.co>
2022-03-06 23:27:16 -0500

    Created Login, Pending Finishing

--- app/src/main/java/com/andresd/socialverse/data/LoginRepository.java
+++ app/src/main/java/com/andresd/socialverse/data/LoginRepository.java
@@ -5,6 +5,7 @@ import com.andresd.socialverse.data.model.LoggedInUser;
 /**
  * Class that requests authentication and user information from the remote data source and
  * maintains an in-memory cache of login status and user credentials information.
+ * FIXME: Adjust for firebase user authentication
  */
 public class LoginRepository {
 
