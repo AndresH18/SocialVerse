08556cf319cc5e66e39f1765b518a01b786dddae
andre <andres.hoyos18@eia.edu.co>
2022-03-08 20:03:39 -0500

    Undo Delete

---
+++ app/src/main/java/com/andresd/socialverse/data/Result.java
@@ -0,0 +1,49 @@
+package com.andresd.socialverse.data;
+
+/**
+ * A generic class that holds a result success w/ data or an error exception.
+ * FIXME: Adjust for firebase user authentication
+ */
+public class Result<T> {
+    // hide the private constructor to limit subclass types (Success, Error)
+    private Result() {
+    }
+
+    @Override
+    public String toString() {
+        if (this instanceof Result.Success) {
+            Result.Success<T> success = (Result.Success<T>) this;
+            return "Success[data=" + success.getData().toString() + "]";
+        } else if (this instanceof Result.Error) {
+            Result.Error error = (Result.Error) this;
+            return "Error[exception=" + error.getError().toString() + "]";
+        }
+        return "";
+    }
+
+    // Success sub-class
+    public final static class Success<T> extends Result<T> {
+        private T data;
+
+        public Success(T data) {
+            this.data = data;
+        }
+
+        public T getData() {
+            return this.data;
+        }
+    }
+
+    // Error sub-class
+    public final static class Error extends Result {
+        private Exception error;
+
+        public Error(Exception error) {
+            this.error = error;
+        }
+
+        public Exception getError() {
+            return this.error;
+        }
+    }
+}
