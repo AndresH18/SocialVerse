e2c39b57ef2bb86c896e8adce316327a1b9ad29d
AndresH18 <70285044+AndresH18@users.noreply.github.com>
2022-03-15 17:41:15 -0500

    Login livedata (#7)
    * Using LiveData on LoginRepository
    
    * Using LiveData on LoginRepository
    
    * Using LiveData on LoginRepository
    
    * Implemented LiveData inside LoginRepository
    
    * Added Sign out functionality

--- app/src/main/java/com/andresd/socialverse/MainActivity.java
+++ app/src/main/java/com/andresd/socialverse/MainActivity.java
@@ -5,18 +5,27 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.andresd.socialverse.data.model.LoggedInUser;
 import com.andresd.socialverse.data.model.LoginRepository;
 import com.andresd.socialverse.databinding.MainActivityBinding;
 import com.andresd.socialverse.ui.login.LoginActivity;
-
+/**
+ * TODO: Ver como usar hilos para que se pueda hacer uso de LoginRepository sin necesidad de usar
+ *  LiveData ahi.
+ *
+ * TODO: Ver como poner lo de SignInViewModel y SignUpViewModel dentro de LoginViewModel y
+ *  compartirlo en los fragmentos.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -34,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
 
         // check if the user is logged in
         // FIXME: UNCOMMENT
-        if (mAuth.getUser() == null) {
+        if (mAuth.getUser().getValue() == null) {
             Log.d(TAG, "onCreate: User is not logged, starting LoginActivity");
             Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
             startActivity(loginIntent);
@@ -47,12 +56,9 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_groups,
                 R.id.navigation_search).build();
-//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build(); Ver si esto también sirve
-
+//        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build(); esto también sirve
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-
         /*        // Used by the default navigation component,
         // No longer required, pending to delete
         if (savedInstanceState == null) {
@@ -60,12 +66,27 @@ public class MainActivity extends AppCompatActivity {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow();
         }*/
+
+        mAuth.getUser().observe(this, new Observer<LoggedInUser>() {
+            @Override
+            public void onChanged(LoggedInUser loggedInUser) {
+                if (loggedInUser == null) {
+                    // TODO: sign out from activity
+                    Toast.makeText(MainActivity.this, "Logged Out", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
     }
 
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        if (item.getItemId() == R.id.sign_out) {
+            mAuth.signOut();
+        }
+        return true;
     }
 
     @Override
