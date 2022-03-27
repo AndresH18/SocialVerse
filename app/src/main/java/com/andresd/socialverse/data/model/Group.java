00737d2324826e87e513c0a56f946012e4e750e7
andre <andres.hoyos18@eia.edu.co>
2022-03-26 22:35:54 -0500

    Created Group data class

---
+++ app/src/main/java/com/andresd/socialverse/data/model/Group.java
@@ -0,0 +1,41 @@
+package com.andresd.socialverse.data.model;
+
+/**
+ *
+ */
+public class Group {
+
+    // attributes
+    private String id;
+    private String name;
+    private String description;
+
+    // required Empty constructor
+    public Group() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+}
