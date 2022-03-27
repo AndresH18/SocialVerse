df008f2b8bcef63926e94d4c48c29cb396907e78
andre <andres.hoyos18@eia.edu.co>
2022-03-26 22:36:20 -0500

    Created Annotation to Distinguish what belongs to Sign Up.

--- app/src/main/java/com/andresd/socialverse/ui/login/SignUpElement.java
+++ app/src/main/java/com/andresd/socialverse/ui/login/SignUpElement.java
@@ -2,6 +2,7 @@ package com.andresd.socialverse.ui.login;
 
 /**
  * <p><b>Interface to mark elements that belong to the sign up context</b></p>
+ * TODO: DELETE ALL ELEMENTS WITH THIS ANNOTATION
  */
 public @interface SignUpElement {
 }
