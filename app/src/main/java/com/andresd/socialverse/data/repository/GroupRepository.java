cd7b9611ff96438f3d1b355873be318371a719ce
andre <andres.hoyos18@eia.edu.co>
2022-03-28 20:36:27 -0500

    Created the way to search for groups by tags or by direct name.
    Made use of FireStore Queries (whereEqual,...)

--- app/src/main/java/com/andresd/socialverse/data/repository/GroupRepository.java
+++ app/src/main/java/com/andresd/socialverse/data/repository/GroupRepository.java
@@ -8,10 +8,16 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class GroupRepository {
 
-    public static final String GROUPS = "groups";
+    private static final String GROUPS = "groups";
+    private static final String TAGS = "tags";
+    private static final String NAME = "name";
 
     private static volatile GroupRepository instance;
 
@@ -26,21 +32,59 @@ public class GroupRepository {
         return instance;
     }
 
-    public void searchGroup(@NonNull String documentString, MutableLiveData<Group> liveData) {
-        FirebaseFirestore.getInstance().collection(GROUPS).document(documentString)
-                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+    public void searchGroupsByTag(@NonNull List<String> groupTags, @NonNull MutableLiveData<List<Group>> liveData) {
+        FirebaseFirestore.getInstance().collection(GROUPS).whereArrayContainsAny(TAGS, groupTags)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        liveData.postValue(document.toObject(Group.class));
-                    } else {
-                        liveData.postValue(null);
+                    QuerySnapshot querySnapshot = task.getResult();
+                    List<Group> groupList = new ArrayList<>();
+                    Group group;
+                    for (DocumentSnapshot document : querySnapshot.getDocuments()) {
+                        group = document.toObject(Group.class);
+                        group.setId(document.getId());
+                        groupList.add(group);
                     }
+                    liveData.postValue(groupList);
                 }
             }
         });
+    }
+
+    public void searchGroupByName(@NonNull String groupName, @NonNull MutableLiveData<List<Group>> liveData) {
+        FirebaseFirestore.getInstance().collection(GROUPS).whereEqualTo(NAME, groupName)
+                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    QuerySnapshot querySnapshot = task.getResult();
+                    List<Group> groupList = new ArrayList<>();
+                    Group group;
+                    for (DocumentSnapshot document : querySnapshot.getDocuments()){
+                        group = document.toObject(Group.class);
+                        group.setId(document.getId());
+                        groupList.add(group);
+                    }
+                    liveData.postValue(groupList);
+                }
+            }
+        });
+
+//        FirebaseFirestore.getInstance().collection(GROUPS).document(groupName)
+//                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if (task.isSuccessful()) {
+//                    DocumentSnapshot document = task.getResult();
+//                    if (document.exists()) {
+//                        liveData.postValue(document.toObject(Group.class));
+//                    } else {
+//                        liveData.postValue(null);
+//                    }
+//                }
+//            }
+//        });
 
 //        return groupLiveData;
     }
--- app/src/main/java/com/andresd/socialverse/ui/main/search/SearchFragment.java
+++ app/src/main/java/com/andresd/socialverse/ui/main/search/SearchFragment.java
@@ -13,10 +13,14 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.andresd.socialverse.data.model.Group;
 import com.andresd.socialverse.databinding.FragmentSearchBinding;
+import com.andresd.socialverse.ui.adapters.GroupRecyclerAdapter;
+
+import java.util.List;
 
 
 public class SearchFragment extends Fragment {
@@ -24,8 +28,8 @@ public class SearchFragment extends Fragment {
     private FragmentSearchBinding binding;
     private SearchViewModel mViewModel;
 
-//    private RecyclerView.LayoutManager layoutManager;
-//    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private RecyclerView.Adapter<GroupRecyclerAdapter.ViewHolder> adapter;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -45,44 +49,59 @@ public class SearchFragment extends Fragment {
 //            }
 //        });
 
+
         /* Create Recycle View Element */
         // create layout manager for RecyclerView
-//        layoutManager = new LinearLayoutManager(requireContext());
-//        // set RecyclerView's layout manager
-//        binding.recyclerView.setLayoutManager(layoutManager);
-//        // create RecyclerView Adapter
-//        adapter = new GroupRecyclerAdapter();
-//        // set RecyclerView's adapter
-//        binding.recyclerView.setAdapter(adapter);
+        layoutManager = new LinearLayoutManager(requireContext());
+        // set RecyclerView's layout manager
+        binding.groupsRecyclerView.setLayoutManager(layoutManager);
+        // create RecyclerView Adapter
+        adapter = new GroupRecyclerAdapter();
+        // set RecyclerView's adapter
+        binding.groupsRecyclerView.setAdapter(adapter);
 
-        /* Observe ViewModel Group changes */
-        mViewModel.getGroupLiveData().observe(getViewLifecycleOwner(), new Observer<Group>() {
+
+        mViewModel.getGroups().observe(getViewLifecycleOwner(), new Observer<List<Group>>() {
             @Override
-            public void onChanged(Group group) {
+            public void onChanged(List<Group> groups) {
                 binding.progressBar.setVisibility(View.GONE);
-                if (group != null) {
-//                    binding.groupCard.groupCardView.setVisibility(View.VISIBLE);
-//                    binding.groupCard.itemTitle.setText(group.getName());
-//                    binding.groupCard.itemDetail.setText(group.getDetail());
-
-                    SearchFragmentDirections.NavigateToGroupActivity directions = SearchFragmentDirections.navigateToGroupActivity(group.getName());
-                    Navigation.findNavController(requireView()).navigate(directions);
-                }
+                GroupRecyclerAdapter a = (GroupRecyclerAdapter) adapter;
+                a.setGroupList(groups);
             }
         });
+
+
+        /* Observe ViewModel Group changes FIXME */
+//        mViewModel.getGroupLiveData().observe(getViewLifecycleOwner(), new Observer<Group>() {
+//            @Override
+//            public void onChanged(Group group) {
+//                binding.progressBar.setVisibility(View.GONE);
+//                if (group != null) {
+////                    binding.groupCard.groupCardView.setVisibility(View.VISIBLE);
+////                    binding.groupCard.itemTitle.setText(group.getName());
+////                    binding.groupCard.itemDetail.setText(group.getDetail());
+//
+//                    /* Go to Group */
+////                    SearchFragmentDirections.NavigateToGroupActivity directions = SearchFragmentDirections.navigateToGroupActivity(group.getName());
+////                    Navigation.findNavController(requireView()).navigate(directions);
+//                }
+//            }
+//        });
+
+
         /* Add Click listener to Button */
         binding.searchButton.setOnClickListener(v -> {
 //            ((GroupRecyclerAdapter) adapter).add(
 //                    new String[]{"Peliculas", "Teatro", "Videojuegos"},
 //                    new String[]{"cine", "como los papas de batman", "Yaya, juegos"});
-            searchGroup();
+            search();
         });
         /* Add action listener to EditText */
         binding.searchGroupEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    searchGroup();
+                    search();
                 }
                 return false;
             }
@@ -91,11 +110,18 @@ public class SearchFragment extends Fragment {
         return binding.getRoot();
     }
 
-    private void searchGroup() {
+    private void search() {
         final String searchString = binding.searchGroupEditText.getText().toString().trim().toLowerCase();
         if (!searchString.isEmpty()) {
-            binding.progressBar.setVisibility(View.VISIBLE);
-            mViewModel.searchGroup(searchString);
+            if (binding.tagsCheckBox.isChecked()) {
+                // search by tags
+                binding.progressBar.setVisibility(View.VISIBLE);
+                mViewModel.searchGroupsByTags(searchString);
+            } else {
+                // search by name
+                mViewModel.searchGroupByName(searchString);
+            }
+
         }
     }
 
