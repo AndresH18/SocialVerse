8f19f9cba0ee29f34cdf2d4d9b804b71b02274e1
Andres_asus <andres.hoyos18@eia,edu.co>
2022-03-30 07:40:34 -0500

    Created AbstractPost class

---
+++ app/src/main/java/com/andresd/socialverse/data/model/AbstractPost.java
@@ -0,0 +1,15 @@
+package com.andresd.socialverse.data.model;
+
+public abstract class AbstractPost {
+
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    protected void setId(String id) {
+        this.id = id;
+    }
+
+}
